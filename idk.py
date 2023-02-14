df9dc72cbc577aaed2d1c275277ed2e92d6eb7a8
m7md_sm7a <111310497+7moody9991@users.noreply.github.com>
2023-02-14 17:24:41 +0200

    Add files via upload

---
+++ idk.py
@@ -0,0 +1,14 @@
+from microbit import *
+from ssd1306 import initialize, clear_oled
+from ssd1306_text import add_text
+import radio
+radio.config(group=69)
+radio.on()
+initialize()
+clear_oled()
+while True:
+    message = radio.receive()
+    if message == 'someone detected':
+        add_text(0, 1, "there is someone")
+    elif message == 'nothing':
+        add_text(0, 0, "nothing")
